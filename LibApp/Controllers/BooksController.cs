cecfa2a6a9998aba7626b8f213ad3c73341da7f9
Jakub Kościukiewicz <qubakosciukiewicz@gmail.com>
2021-10-16 12:44:11 +0200

    dodano modele

---
+++ LibApp/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LibApp.Models;
+
+namespace LibApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        private IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Id = 1, Name = "English" };
+            return View(firstBook);
+        }
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
